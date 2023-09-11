26bd01c16cbcb56326b125deeffeb0eb6acee500
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-11 14:12:32 -0700

    Update unit_controller.py

--- unit_controller.py
+++ unit_controller.py
@@ -1,5 +1,5 @@
 def emergency_shut_off():
-    """Checks for dangerous conditions and shuts down operations of robot to optimize damage control. NOTE: DO NOT BYPASS EVER!!!"""
+    """ Checks for dangerous conditions and shuts down operations of robot to optimize damage control. NOTE: DO NOT BYPASS EVER!!!"""
     red_flags = 0
     # Note: may need to use smart contract functionality to ensure some kind of immutable state for this. OR potentially some governance structure.
     
@@ -9,22 +9,35 @@ def emergency_shut_off():
     # 3.) Low power (power source depleted/malfunction) Reason: minimize money loss/ accidents due to poor timing.
     # 4.) Lost (cant find a network and cant figure out where it is) Reason: minimize losses/accidents due to failures/ environmental variables.
     # 5.) Damage/unforseen anomoly (shit happens...) Reason: need to make sure they are useful. broken robots not knowing they're broken would be bad.
+    # 6.) Tampering with internals (opening proprietary access ports/ machine) Reason: don't want bad actors reprogramming the bot :D
     if red_flags == 0:
         return True
     else:
         print("Activating self-destruct sequence... maybe.")
 
 def mimic():
-    """Enables asynchronous computer-vision machine learning algorithm to input Owner activity as training data in real-time."""
+    """ Enables asynchronous computer-vision machine learning algorithm to input Owner activity as training data in real-time."""
     if emergency_shut_off():
         print("Activating mimic protocol.")
         vision_input = load_vision("Cam Config 1") # Activates computer-vision hardware
         vision_model = 'Alpha 1' # Specific machine learning model for the current application. Note: may need to be flexible for application swapping.
+        data_destination = ''
+        license = ''
+        connect_network('mimic', license, data_destination)
     # For updates... follow @nf_qts on X.
     # Have a wonderful day!
     # Help people...
     pass
 
 def load_vision(config):
+    """ Activates computer vision cameras to gather real-world data."""
     print(f"Loading computer vision module {config}")
     pass
+
+def connect_network(purpose, credentials, endpoint):
+    """ Connects unit to the hivemind for 2-way data exchange."""
+    if purpose == 'mimic':
+        # Need to check credentials
+        if credentials:
+            print("Connecting to hivemind.")
+            url = endpoint
