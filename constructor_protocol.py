5830bca0c87a0b4be0426b56d0e9e74c3fd108e2
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-12 19:52:34 -0700

    Added constructor_protocol.py

---
+++ constructor_protocol.py
@@ -0,0 +1,19 @@
+class Constructor:
+    """AI Agent type assigned to construct real world objects. Backbone of the project due to the nature of it."""
+    def __init__(self):
+        self.id = 1
+
+def load_vision(config):
+    """ Activates computer vision cameras to gather real-world data."""
+
+    print(f"Loading computer vision module {config}")
+    pass
+
+def repair_self():
+    pass
+
+def repair_other():
+    pass
+
+def build_parts(part_material):
+    pass
