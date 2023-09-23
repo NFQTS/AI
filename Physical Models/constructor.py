da91843537a62c3bfbd315110489e5400bc457a1
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-22 19:19:23 -0700

    Update constructor.py

--- Physical Models/constructor.py
+++ Physical Models/constructor.py
@@ -1,1 +1,14 @@
 # Bot meant to 3D print bot parts
+class Constructor:
+    """AI Agent type assigned to construct real-world objects, mostly other robots. Backbone of the project due to the nature of it."""
+    def __init__(self):
+        self.id = 1
+
+def repair_self():
+    pass
+
+def repair_other():
+    pass
+
+def build_parts(part_material):
+    pass
