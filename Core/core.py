27e10730e179293760d82d9802d69670ff56847e
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-23 00:37:35 -0700

    created Robot class in core.py

--- Core/core.py
+++ Core/core.py
@@ -1,61 +1,82 @@
-def emergency_shut_off():
-    """ Checks for dangerous conditions and shuts down operations of robot to optimize damage control. NOTE: DO NOT BYPASS EVER!!!"""
-    red_flags = 0
-    # Note: may need to use smart contract functionality to ensure some kind of immutable state for this. OR potentially some governance structure.
+class Robot:
+    def __init__(self, id_number, robot_type):
+        self.id_number = id_number  # For hivemind use
+        self.task_queue = ["setup"]  # Initialize as an empty list
+        self.sensor_statuses = {}  # Needs to be able to store data about system health and environment.
+        self.onboard_libraries = {}  # For storing custom protocols/functionality
+        self.robot_type = robot_type  # To easily connect the bot to the correct APIs/services/functions
+        self.owner = ''  # To make sure the appropriate person is benefiting from the bot.
+
+    def emergency_shut_off(self):
+        """ Checks for dangerous conditions and shuts down operations of robot to optimize damage control."""
+        # Note: may need to use smart contract functionality to ensure some kind of immutable state for this. OR potentially some governance structure.
+        # Abort conditions:
+        # 1.) temperature anomaly (electrical fire/ etc) Reason: minimize chance of fire/damage. Other mitigation: onboard firefighting capabilities.
+        # 2.) AWOL protocol (AI somehow becomes self aware/ damaging) Reason: minimize chance of human loss. Other mitigation: protocols for network, etc.
+        # 3.) Low power (power source depleted/malfunction) Reason: minimize money loss/ accidents due to poor timing.
+        # 4.) Lost (cant find a network and cant figure out where it is) Reason: minimize losses/accidents due to failures/ environmental variables.
+        # 5.) Damage/unforseen anomoly (shit happens...) Reason: need to make sure they are useful. broken robots not knowing they're broken would be bad.
+        # 6.) Tampering with internals (opening proprietary access ports/ machine) Reason: don't want bad actors reprogramming the bot :D
     
-    # Abort conditions:
-    # 1.) temperature anomaly (electrical fire/ etc) Reason: minimize chance of fire/damage. Other mitigation: onboard firefighting capabilities.
-    # 2.) AWOL protocol (AI somehow becomes self aware/ damaging) Reason: minimize chance of human loss. Other mitigation: protocols for network, etc.
-    # 3.) Low power (power source depleted/malfunction) Reason: minimize money loss/ accidents due to poor timing.
-    # 4.) Lost (cant find a network and cant figure out where it is) Reason: minimize losses/accidents due to failures/ environmental variables.
-    # 5.) Damage/unforseen anomoly (shit happens...) Reason: need to make sure they are useful. broken robots not knowing they're broken would be bad.
-    # 6.) Tampering with internals (opening proprietary access ports/ machine) Reason: don't want bad actors reprogramming the bot :D
-    if red_flags == 0:
-        return True
-    else:
-        print("Activating self-destruct sequence... maybe.")
+        red_flags = self.calculate_red_flags()
+        
+        if red_flags == 0:
+            return True
+        else:
+            print("Activating self-destruct sequence... maybe.")
 
+    def check_status(self):
+        """ Runs a check of all sensors and systems to identify issues."""
+        if "setup" in self.task_queue:
+            print("Setting up bot now...")
+            self.task_queue.remove("setup")
+            print("Setup complete...")
+        print("Checking status now...")
+        # Add logic to check sensor statuses and other conditions
+        # 1.) are sensors turned on?
+        # 2.) are all sensors working?
 
-def check_status():
-    """ Runs a check of all sensors and systems to identify issues."""
-    pass
+    def activate_sensors(self, config):
+        """ Activates computer vision cameras and other environmental sensor systems to gather real-world data."""
+        print("Activating sensory systems...")
+        # Add logic to activate sensors based on the 'config' parameter
 
-def activate_sensors(config):
-    """ Activates computer vision cameras and other environmental sensor systems to gather real-world data."""
-    pass
-
-def connect_network(purpose, credentials, endpoint):
-    """ Connects unit to the hivemind for 2-way data exchange to mimic user."""
-    if purpose == 'mimic':
-        # Need to check credentials
-        print("Beginning mimic protocol.")
-
-    elif purpose == 'owner_check':
-        print("Checking Owner licensing.")
-    if credential_check(credentials, purpose):
+    def connect_network(self, purpose, credentials):
+        """ Connects unit to the hivemind for 2-way data exchange."""
+        if self.credential_check(credentials, purpose):
+            if purpose == 'owner_check':
+                print("Checking Owner licensing.")
             print(f"Connecting to hivemind to execute the {purpose} function.")
-            url = endpoint
-   
-def credential_check(auth, task):
-    print(f"Checking credentials for {task}")
-    pass
+            url = f"https://hivemind.nfqts.com/api/v1?{purpose}"
 
-def maintenance():
-    """ Runs a maintenance protocol that handles charging, and sends a request for service to hivemind."""
-    pass
+    def credential_check(self, auth, task):
+        print(f"Checking credentials for {task}")
+        # Add logic to check credentials
 
-def owner_check():
-    """ Authenticates the owner and robot are authorized for the current use."""
-    pass
+    def maintenance(self):
+        """ Runs a maintenance protocol that handles charging, and sends a request for service to hivemind."""
+        # Add maintenance logic
 
-def shutdown():
-    """ Turn off the robot systems optimally."""
-    pass
+    def owner_check(self):
+        """ Authenticates the owner and robot are authorized for the current use."""
+        # Add owner authentication logic
 
-def restart():
-    """ 'Have you tried turning it off and on again?' - The IT Crowd."""
-    pass
+    def update(self):
+        """ Update firmware, perform physical self-upgrading tasks if the model is capable."""
+        # Add update logic
 
-def update():
-    """ Update firmware, perform physical self-upgrading tasks if the model is capable."""
-    pass
+    def shutdown(self):
+        """ Turn off the robot systems optimally."""
+        # Add shutdown logic
+
+    def restart(self):
+        """ 'Have you tried turning it off and on again?' - The IT Crowd."""
+        # Add restart logic
+
+    def calculate_red_flags(self):
+        """ Calculate red flags for emergency shut-off based on sensor data and conditions."""
+        self.check_status()
+        # Add your red flag calculation logic here
+        return 0  # Placeholder for now
+
+    
