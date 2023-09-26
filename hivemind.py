1e26e7d605eb9735d64863b1b97293e8b365a19e
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-26 15:53:25 -0700

    Create hivemind.py

---
+++ hivemind.py
@@ -0,0 +1,20 @@
+def validate_credentials():
+    """ Standard auth protocol. """
+    pass
+
+def check_owner():
+    """ Get data about the unit Owner. """
+    pass
+
+def check_versions():
+    """ Check to see if a unit needs updates. """
+    pass
+
+def send_updates():
+    """ Send required updates to units. """
+    pass
+
+def mint_data():
+    """ Stores important bot-related data on-chain to allow for public auditing. """
+    # We are so back lol
+    pass
