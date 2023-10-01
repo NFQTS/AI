a8952ea5d86aa50bf3e48ee706c9b66b5cb13a63
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-10-01 09:07:03 -0700

    Added functions and docs to hivemind.py

--- hivemind.py
+++ hivemind.py
@@ -1,5 +1,12 @@
-def validate_credentials():
-    """ Standard auth protocol. """
+def validate_credentials(auth, task):
+    """ Standard auth protocol. 
+    Inputs 
+    Auth: JSON
+    Task: string
+    
+    Returns
+    Boolean """
+    
     pass
 
 def check_owner():
@@ -17,4 +24,12 @@ def send_updates():
 def mint_data():
     """ Stores important bot-related data on-chain to allow for public auditing. """
     # We are so back lol
+    pass
+
+def optimize_ml_models():
+    """ Handles the A/B testing/ AUTML capabilities of the system to maximize reliability. """
+    pass
+
+def archive_data():
+    """ Saves relevant data to our data lakes """
     pass
